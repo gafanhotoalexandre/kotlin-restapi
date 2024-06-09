42a3b4f63199d96142152697caf94b05c7e79170
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2024-06-09 02:55:59 -0300

    initial commit

---
+++ FirstSteps/kotlin-restapi/src/test/kotlin/github/com/gafanhotoalexandre/StartupTests.kt
@@ -0,0 +1,13 @@
+package github.com.gafanhotoalexandre
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
